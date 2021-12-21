c533411289999d5cbd782c090251bd643af04387
Andrew Peterson <andrewp2@umbc.edu>
2021-12-21 12:19:16 -0500

    Revert "use prisma orm"

--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -1,47 +1,37 @@
 import type { Request } from "@sveltejs/kit"
-import PrismaClient from "$lib/prisma"
 
-const prisma = new PrismaClient();
+// TODO: Persist in database
+let todos: Todo[] = [];
 
-export const api = async (request: Request, data?: Record<string, unknown>) => {
+export const api = (request: Request, data?: Record<string, unknown>) => {
     let body = {};
     let status = 500;
     
     
     switch (request.method.toUpperCase()) {
         case "GET":
-            body = await prisma.todo.findMany();
+            body = todos;
             status = 200;
             break;
         case "POST":
-            body = await prisma.todo.create({
-                data: {
-                    created_at: data.created_at as Date,
-                    done: data.done as boolean,
-                    text: data.text as string
-                }
-            })
+            todos.push(data as Todo);
+            body = data;
             status = 201;
             break;
         case "DELETE":
-            body = await prisma.todo.delete({
-                where: {
-                    uid: request.params.uid
-                }
-            })
+            todos = todos.filter(todo => todo.uid != request.params.uid)
             status = 200;
             break;
         case "PATCH":
-            body = await prisma.todo.update({
-                where: {
-                    uid: request.params.uid
-                },
-                data: {
-                    done: data.done,
-                    text: data.text
+            status = 200
+            todos = todos.map(todo => {
+                if (todo.uid === request.params.uid) {
+                   if (data.text) todo.text = data.text as string;
+                   else todo.done = data.done as boolean;
                 }
-            })
-            status = 200;
+                return todo;
+            });
+            body = todos.find(todo => todo.uid === request.params.uid);
             break;
         default:
             break;
